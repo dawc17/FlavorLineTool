0f3a0f71050fedc81e300f78df7806696566da75
Dawid Krystian Czaplicki <100421659+dawc17@users.noreply.github.com>
2026-01-07 13:38:28 +0100

    readme and main file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FlavorLineTool
 a cli app for tracking cookies and interacting with flavortown
+
+## features
+- ability to log your cookies with commands such as
+'''
+flavor cookies add [number]
+flavor cookies remove [number]
+flavor cookies set [number]
+'''
+- interact with the flavortown api (tbd)
+- check total time spent on your projects
---
+++ main.py
@@ -0,0 +1,22 @@
+# put code here or some shit
+# her name was lola she was a showgirl
+# with yellow feathers in her hair and a dress cut down to there
+# she would merengue and do the cha-cha
+# and while she tried to be a star
+# Tony always tended bar
+# and they called him "Café Caramba"
+# and he liked to flirt with Lola
+# copa
+# tequila, dancing with Lola
+# until they kicked us out of Copa
+# ooh, lola, lola, lola, lo-lo-lo-lo-lo-la
+# lo-lo-lo-lo-lo-la
+# lo-lo-lo-lo-lo-la
+# lo-lo-lo-lo-lo-la
+# lo-lo-lo-lo-lo-la
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
