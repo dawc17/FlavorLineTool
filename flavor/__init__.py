a2a580840caaf01c41c4fcab1773262bace4ae9d
Dawid Krystian Czaplicki <100421659+dawc17@users.noreply.github.com>
2026-01-07 13:50:45 +0100

    more project structure + cli callback and a simple print function

---
+++ flavor/__init__.py
@@ -0,0 +1,8 @@
+# put code here or some shit
+# lo-lo-lo-lo-lo-la
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
---
+++ flavor/cli.py
@@ -0,0 +1,22 @@
+# flavor/cli.py
+import typer
+from rich.console import Console
+
+app = typer.Typer()
+console = Console()
+
+@app.callback()
+def callback():
+    """
+    FlavorLineTool - A CLI for tracking cookies and interacting with flavortown.
+    """
+
+@app.command()
+def status():
+    console.print("Flavortown CLI is alive", style="bold green")
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    main()
